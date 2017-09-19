b42b6df035d1e0f224423721781df9fc249daa61
Serra Park <serra.park@berkeley.edu>
2017-09-19 14:10:31 -0700

    submit hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    i = a.map{|element| element.to_i}
+    i = i.map{|number| number + 2}
+    i = i.select{|number| number % 2 == 0}
+    i = i.uniq
+    i = i.select{|number| number <= 10}
+    return i.inject(0, &:+)
   end
 end
 
